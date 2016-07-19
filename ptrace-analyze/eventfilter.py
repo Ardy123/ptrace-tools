2a6cabd496cfe4968e24fc3379243a7c4ba323f1
Ardavon Falls <ardy@tokbox.com>
2016-07-19 14:20:11 -0700

    Cleaned up eventfilter

--- ptrace-analyze/eventfilter.py
+++ ptrace-analyze/eventfilter.py
@@ -1,39 +1,33 @@
 class EventFilter:
     START_TAG = 0
     END_TAG = 1
-    
-    _startEventTbl = [
-        'PROCESS_START', 
-        'PTHREAD_START', 
-        'PTHREAD_MUTEX_LOCK_LEAVE', 
-        'PTHREAD_COND_WAIT_LEAVE',
-        'PTHREAD_COND_WAIT_TIMEOUT']
-    _endEventTbl = [
-        'PROCESS_END',
-        'PTHREAD_END',
-        'PTHREAD_MUTEX_LOCK_ENTER',
-        'PTHREAD_COND_WAIT_ENTER']
+    _eventTbl = {
+        'PROCESS_START' : 0,  
+        'PTHREAD_START' : 0, 
+        'PTHREAD_MUTEX_LOCK_LEAVE' : 0, 
+        'PTHREAD_COND_WAIT_LEAVE' : 0,
+        'PTHREAD_COND_WAIT_TIMEOUT': 0,
+        'PROCESS_END' : 1,
+        'PTHREAD_END' : 1,
+        'PTHREAD_MUTEX_LOCK_ENTER' : 1,
+        'PTHREAD_COND_WAIT_ENTER' : 1}
     
     def __init__(self, source):
         self._source = source
-        self._iter = source.iter(EventFilter._startEventTbl + EventFilter._endEventTbl)
-        self._eventTbl = dict(
-            [(i, EventFilter.START_TAG) for i in EventFilter._startEventTbl] +
-            [(j, EventFilter.END_TAG) for j in EventFilter._endEventTbl])
+        self._iter = source.iter(EventFilter._eventTbl.keys())
         
     def __iter__(self):
         return self
     
     def next(self):
-        line = self._iter.next()
-        event = self._filterEvent(*line.split())
+        event = self._filterEvent(*self._iter.next().split())
         return event
 
     def close(self):
         self._source.close()
         
     def _filterEvent(self, event, tid, time):
-        return (self._eventTbl[event], tid, EventFilter._calcUSec(time), event)
+        return (EventFilter._eventTbl[event], tid, EventFilter._calcUSec(time), event)
 
     @staticmethod
     def _calcUSec(time):
