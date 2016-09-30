52ad2c356e68b2f40d979a63db21d4b64f56fef8
ardy <afalls@qvxlabs.com>
2016-09-30 02:29:29 -0700

    improved threadsource indexing performance

--- ptrace-gui/threadsource.py
+++ ptrace-gui/threadsource.py
@@ -7,26 +7,20 @@ class ThreadSource:
     def __init__(self, strm):
         self._strm = strm
         self._memmap = mmap.mmap(strm.fileno(), 0, prot=mmap.PROT_READ)
+        # setup methods
         self.find = self._memmap.find
         self.rfind = self._memmap.rfind
+        self.__getitem__ = self._memmap.__getitem__
+        self.__len__ = self._memmap.size
 
     def __iter__(self):
         return self.findIter('\n')
-
-    def __len__(self):
-        return self._memmap.size()
-
-    def __getitem__(self, index):
-        return self._memmap[index]
 
     def regexIter(self, pattern):
         return re.finditer(pattern, self._memmap)
 
     def findIter(self, pattern):
         return self._findGenerator(pattern)
-
-    def find(self, pattern, start, end):
-        return self._
 
     def close(self):
         self._memmap.close()
